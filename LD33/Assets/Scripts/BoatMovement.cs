a51439a9f5fd0f40e9240d187a649810eba78d0d
Rory O'Connor <r.angus.oconnor@gmail.com>
2015-08-22 20:26:19 -0400

    AI Functional on boats

--- LD33/Assets/Scripts/BoatMovement.cs
+++ LD33/Assets/Scripts/BoatMovement.cs
@@ -10,9 +10,13 @@ public abstract class BoatMovement : MonoBehaviour
     public Vector2 dest;
     public Renderer water;
     public GameObject sprite;
-    public GameObject direction;
+    public GameObject forwardDir;
+    public GameObject reverseDir;
 
     public bool colliding;
+    public bool reversing;
+    public float reverseTime;
+    private float timer;
 
     void OnDrawGizmosSelected()
     {
@@ -23,11 +27,11 @@ public abstract class BoatMovement : MonoBehaviour
     public void Move()
     {
         // Move in a forwars direction
-        transform.position = Vector2.Lerp(transform.position, direction.transform.position, moveSpd * Time.deltaTime);
+        transform.position = Vector2.Lerp(transform.position, forwardDir.transform.position, moveSpd * Time.deltaTime);
 
         AngleCorrection();
 
-        if(Vector2.Distance(transform.position, dest) < 5)
+        if(Vector2.Distance(transform.position, dest) < 10)
         {
             dest = NewPoint();
         }
@@ -36,14 +40,24 @@ public abstract class BoatMovement : MonoBehaviour
 
     public void AvoidCollision()
     {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            transform.position = Vector2.Lerp(transform.position, reverseDir.transform.position, moveSpd * Time.deltaTime);
+        }
+        else
+        {
+            reversing = false;
+        }
+
         AngleCorrection();
     }
 
     public Vector2 NewPoint()
     {
         Vector2 newPoint = Vector2.zero;
-        newPoint.x = Random.Range(water.transform.position.x - (water.bounds.size.x * 0.5f), water.transform.position.x + (water.bounds.size.x * 0.5f));
-        newPoint.y = Random.Range(water.transform.position.y - (water.bounds.size.y * 0.5f), water.transform.position.y + (water.bounds.size.y * 0.5f));
+        newPoint.x = Random.Range(water.transform.position.x - (water.bounds.size.x * 0.45f), water.transform.position.x + (water.bounds.size.x * 0.45f));
+        newPoint.y = Random.Range(water.transform.position.y - (water.bounds.size.y * 0.45f), water.transform.position.y + (water.bounds.size.y * 0.45f));
 
         return newPoint;
     }
@@ -73,6 +87,9 @@ public abstract class BoatMovement : MonoBehaviour
     void OnCollisionEnter(Collision other)
     {
         colliding = true;
+        reversing = true;
+        timer = reverseTime;
+        dest = NewPoint();
     }
 
     void OnCollisionExit(Collision other)
--- LD33/Assets/Scripts/Ship.cs
+++ LD33/Assets/Scripts/Ship.cs
@@ -12,9 +12,19 @@ public class Ship : BoatMovement
 
     void Update ()
     {
-        if (!colliding)
+        if (!reversing)
             Move();
         else
             AvoidCollision();
+
+
+        // Deals with ships going off screen. Players will just think they lost their chance
+        if(transform.position.x < (water.transform.position.x - water.bounds.size.x * 0.50f) &&
+            transform.position.x > (water.transform.position.x + water.bounds.size.x * 0.50f) &&
+            transform.position.y < (water.transform.position.y - water.bounds.size.y * 0.50f) &&
+            transform.position.y > (water.transform.position.y + water.bounds.size.y * 0.50f))
+        {
+            Destroy(gameObject);
+        }
 	}
 }
